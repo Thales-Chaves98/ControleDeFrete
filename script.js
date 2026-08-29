dd8894d61aad8bf947d4171cc3abd93261c87cc8
Thales <thales.augusto.chaves98@gmail.com>
2026-08-28 23:39:43 -0300

    criacao da renderizacao do obj frete

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const temaBtn = document.getElementById("theme-btn");
 const inputFrete = document.getElementById("text-input");
 const addFreteBtn = document.getElementById("add-frete-btn");
 
+const freteContainer = document.getElementById("fretes-container");
 
 let fretes = [];
 
@@ -10,6 +11,8 @@ let fretes = [];
 
 addFreteBtn.addEventListener('click', (e) => {
     e.preventDefault();
+
+    criarObjetoFrete();
 
 });
 
@@ -33,11 +36,50 @@ function criarObjetoFrete(){
     }
 
     fretes.push(frete);
+    criarTabelaFrete();
+    console.log(fretes);
 
 }
 
 function criarTabelaFrete(){
-    
+    if(fretes.length > 0){
+        freteContainer.innerHTML = '';
+        
+        const tabelaFrete = document.createElement("table");
+        tabelaFrete.classList.add("tabela-frete");
+        
+        const tabelaHeader = document.createElement("thead");
+        tabelaHeader.classList.add("tabela-header");
+
+        const trHeader = document.createElement("tr");
+        
+        const colunas = ["FRETE", "PEDIDO", "FEITO", "AÇÕES"];
+        
+        colunas.forEach((titulo) =>{
+            const th = document.createElement("th");
+
+            th.textContent = titulo;
+            trHeader.appendChild(th);
+            
+        });
+
+        tabelaHeader.appendChild(trHeader);
+       
+        const tabelaBody = document.createElement("tbody");
+        tabelaBody.classList.add("tabela-body");
+
+        fretes.forEach(frete =>{
+            const trBody =document.createElement("tr");
+
+            
+
+        });
+
+        tabelaFrete.appendChild(tabelaHeader);
+        tabelaFrete.appendChild(tabelaBody);
+
+        freteContainer.appendChild(tabelaFrete);
+    }
 }
 
 function gerarId(){
